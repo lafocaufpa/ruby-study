a14ae098881e38a9b353354231bd75fb287ed9cb
Edinaldo Henriques <edinaldohenriques02@gmail.com>
2023-05-22 18:45:01 -0300

    criacao-classes

---
+++ aluno.rb
@@ -0,0 +1,46 @@
+class Aluno < Pessoa
+    include Pessoa
+    attr_reader :matricula, :dataNascimento
+
+    def initialize(nome, sobrenome, idade, rg, matricula, dataNascimento)
+      @nome = nome
+      @sobrenome = sobrenome
+      @idade = idade
+      @rg = rg
+      @matricula = matricula
+      @dataNascimento = dataNascimento
+      @disciplina = nil
+    end
+
+
+    #Getters Methods
+    def getMatricula
+        return @matricula
+    end
+
+    def getDataNascimento
+        return @dataNascimento
+    end
+
+    def getDisciplina
+        if disciplina.nil?
+            puts "Ops! Ainda não tem disciplina cadastrada"
+        else
+            return @disciplina
+        end
+    end
+
+    #Setters Methods
+    def setMatricula=(matricula)
+        @matricula = matricula
+    end
+
+    def setDataNascimento=(dataNascimento)
+        @dataNascimento = dataNascimento
+    end
+
+    #Método para associar um Aluno à uma turma
+    def associar_disciplina(disciplina)
+        @disciplina = disciplina
+    end
+end
---
+++ professor.rb
@@ -0,0 +1,45 @@
+class Professor < Pessoa
+    include Pessoa
+    attr_reader :salario, :cargaHoraria, :idFuncionario
+
+    def initialize(nome, sobrenome, idade, rg, salario, cargaHoraria, idFuncionario)
+      @nome = nome
+      @sobrenome = sobrenome
+      @idade = idade
+      @rg = rg 
+      @salario = salario
+      @cargaHoraria = cargaHoraria
+      @idFuncionario = idFuncionario
+      @turma = nil
+    end
+
+    #Getter Methodturma
+    def getSalario
+        return @salario
+    end
+
+    def getCargaHoraria
+        return @cargaHoraria
+    end
+
+    def getIdFuncionario
+        return @idFuncionario
+    end
+
+    def getTurma
+        return @turma 
+    end
+
+    #Setters Methods
+    def setSalario=(salario)
+        @salario = salario.to_f
+    end
+
+    def setCargaHoraria=(cargaHoraria)
+        @cargaHoraria = cargaHoraria
+    end
+    
+    def setTurma=(turma)
+        @turma = turma
+    end    
+end
