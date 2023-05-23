868cbacdac8b0d44d992c199faa9c2c34b385658
c3s4r_ <cesarlobato27@gmail.com>
2023-05-22 22:50:13 -0300

    correcao-lista

--- listas.rb
+++ listas.rb
@@ -21,19 +21,28 @@ module Cadastro
         id = gets.chomp.to_i
 
         pessoa = Pessoa.new(nome, id)
-        list_pessoa = []
-        list_pessoa << pessoa
+        @list_pessoas ||= []
+        @list_pessoas << pessoa
     end
 
 end
 
 class Exemplo
 include Cadastro
+
+    def listar_pessoas
+        if @list_pessoas.nil? || @list_pessoas.empty?
+            puts "Nenhuma pessoa cadastrada."
+          else
+            @list_pessoas.each do |pessoa|
+              puts pessoa.nome
+            end
+        end
+    end
 end
 
 exemplo = Exemplo.new
 
 exemplo.cadastrar
 
-
-
+exemplo.listar_pessoas
